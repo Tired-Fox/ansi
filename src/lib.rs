f1a7867583efb18b9cf09b0c1837612d2d5f38f7
Zachary Boehm <zboehm@menard-inc.com>
2023-02-21 12:08:09 -0600

    Add macro for parsing and printing saiml

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
 #![doc = include_str!("../README.md")]
 pub mod style;
 pub mod markup;
+
+use markup::Parser;
+
+#[macro_export]
+macro_rules! saiml {
+    ($text:literal) => {
+        Parser::from($text)   
+    };
+    () => { String::new() }
+}
+
+#[macro_export]
+macro_rules! print_saiml {
+    ($text:literal) => {
+        println!("{}", Parser::from($text))  
+    };
+    () => { println!() }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
-mod markup;
-use markup::Parser;
-use markup::types::{Types, FormatState, Command, Color};
-
-mod style;
-use style::{Color as Style_Color, Style};
+use ansi::markup::Parser;
+use ansi::{saiml, print_saiml};
 
 fn main() {
     // Hel[@Fred$]lo W*or_ld[@$]*!
 
-    let sample_ast = vec![
-        Types::Text("Hel".to_string()),
-        Types::Commands(vec![Command::Color(Color::Foreground("red".to_string()))]),
-        Types::Text("lo W".to_string()),
-        Types::Bold(FormatState::On),
-        Types::Text("or".to_string()),
-        Types::Underline(FormatState::On),
-        Types::Text("ld".to_string()),
-        Types::Commands(vec![Command::Color(Color::Reset)]),
-        Types::Bold(FormatState::Off),
-        Types::Text("!".to_string()),
-    ];
-    println!("{:?}", sample_ast);
-
     let result = Parser::from("Hel[@Fred@Bwhite$~https://example.com]lo W*or_ld[@$]* Bold Text!");
     println!("{}", result);
 
-    let style = Style::new()
-        .bold()
-        .foreground(Style_Color::hex("#D60620").unwrap());
-    println!("{}", style.format("Hello World!"))
+    print_saiml!("[@Fblue]*_Hello World!")
 }
